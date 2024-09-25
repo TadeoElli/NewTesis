030287d0bf4788c3f9a1ee209f8bbe9e9f199fc6
taio1000 <tadeoelli@hotmail.com>
2024-09-25 16:40:56 -0300

    FixLevel + build

--- Trascendentales/Assets/Scripts/MouseController.cs
+++ Trascendentales/Assets/Scripts/MouseController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using static Unity.VisualScripting.Dependencies.Sqlite.SQLite3;
 
 public class MouseController : MonoBehaviour
 {

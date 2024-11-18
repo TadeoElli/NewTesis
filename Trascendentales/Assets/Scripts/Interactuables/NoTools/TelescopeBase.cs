d2f76cd8171306833da376b7a8fb20d60d5cfdb4
taio1000 <tadeoelli@hotmail.com>
2024-11-17 22:58:49 -0300

    telescope

---
+++ Trascendentales/Assets/Scripts/Interactuables/NoTools/TelescopeBase.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class TelescopeBase : MonoBehaviour
+{
+    private const string rechargableLayerName = "Rechargable"; // Nombre de la capa Rechargable
+    [SerializeField] Telescope telescope;
+    private void OnCollisionEnter(Collision other)
+    {
+        // Activa el telescopio si colisiona con un objeto de la capa Rechargable
+        if (other.gameObject.layer == 9)
+        {
+            telescope.SetCharged(true);
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        // Desactiva el telescopio al salir de la colisión con un objeto Rechargable
+        if (other.gameObject.layer == 9)
+        {
+            telescope.SetCharged(false);
+        }
+    }
+
+}
+

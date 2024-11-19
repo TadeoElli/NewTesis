df90db46bc96b7a726278fb10e781a0390a83f39
taio1000 <tadeoelli@hotmail.com>
2024-11-19 01:16:40 -0300

    comit

--- Trascendentales/Assets/Scripts/Interactuables/NoTools/Torbellino.cs
+++ Trascendentales/Assets/Scripts/Interactuables/NoTools/Torbellino.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Torbellino : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float baseForce = 10f; // Fuerza base del torbellino
+    [SerializeField] private Transform pointDirection;
+    private float currentForce = 0f;
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        // Verificamos si el objeto que entra es el jugador
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody playerRb = other.GetComponent<Rigidbody>();
+
+            if (playerRb != null)
+            {
+                // Calculamos la dirección del torbellino (hacia donde apunta)
+                Vector3 launchDirection = (pointDirection.position - transform.position).normalized;
+
+                // Calculamos la fuerza según la escala del torbellino
+                float scaleFactor = transform.localScale.magnitude; // Tamaño proporcional
+                currentForce = baseForce * scaleFactor;
+
+                // Aplicamos la fuerza al jugador
+                playerRb.AddForce(launchDirection * currentForce, ForceMode.Force);
+
+                Debug.Log($"Jugador lanzado con fuerza {currentForce} en dirección {launchDirection}");
+            }
+        }
     }
 }
+
